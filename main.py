f540aaacae22c2325f1e40df7a78da5896b27459
Ankit Vani <a@nevitus.org>
2017-02-28 20:55:17 -0500

    separate some common stuff into util.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+import util
 
 
 class Actor(nn.Module):
@@ -105,50 +106,6 @@ class Critic(nn.Module):
         return costs * Variable(self.discount, requires_grad=False)
 
 
-def weights_init(m):
-    def linear_init(weight):
-        fan_out, fan_in = weight.size()
-        weight.data.normal_(0.0, np.sqrt(2.0 / (fan_in + fan_out)))
-    if isinstance(m, nn.Linear):
-        linear_init(m.weight)
-    elif isinstance(m, nn.GRUCell):
-        linear_init(m.weight_ih)
-        linear_init(m.weight_hh)
-    elif isinstance(m, nn.GRU):
-        for param in m.parameters():
-            if len(param.size()) == 2:
-                linear_init(param)
-                linear_init(param)
-    elif isinstance(m, nn.Embedding):
-        m.weight.data.uniform_()
-
-
-def get_toy_data_words(batch_size, seq_len, vocab_size):
-    '''Generate very simple toy training data. Generates sequences of integers where a 'word' is
-       consecutive increasing integers and 0 separates words.'''
-    batch = np.zeros([batch_size, seq_len], dtype=np.int)
-    cur_word = np.random.randint(1, vocab_size, size=batch_size, dtype=np.int)
-    batch[:, 0] = cur_word
-    for i in xrange(1, seq_len):
-        zero_mask = cur_word == 0
-        cur_word += 1
-        cur_word[cur_word > vocab_size-1] = 0
-        cur_word *= np.random.binomial(np.ones(batch_size, dtype=np.int), 0.75)
-        cur_word[zero_mask] = np.random.randint(1, vocab_size, size=np.sum(zero_mask),
-                                                dtype=np.int)
-        batch[:, i] = cur_word
-    return batch
-
-
-def get_toy_data_longterm(batch_size, seq_len, vocab_size):
-    '''Generate simple toy training data where two tokens appear separated by large number
-       of 0's.'''
-    batch = np.zeros([batch_size, seq_len], dtype=np.int)
-    batch[:, int(0.33 * seq_len)] = np.random.randint(1, vocab_size, size=batch_size, dtype=np.int)
-    batch[:, int(0.8 * seq_len)] = np.random.randint(1, vocab_size, size=batch_size, dtype=np.int)
-    return batch
-
-
 if __name__ == '__main__':
     # TODO implement curriculum learning to increase gamma over time.
     #      exponentially saturate to the final gamma value or linearly interpolate?
@@ -190,15 +147,15 @@ if __name__ == '__main__':
     np.set_printoptions(precision=4, threshold=10000, linewidth=200, suppress=True)
 
     if opt.task == 'words':
-        get_data = get_toy_data_words
+        get_data = util.get_toy_data_words
     elif opt.task == 'longterm':
-        get_data = get_toy_data_longterm
+        get_data = util.get_toy_data_longterm
     else:
         print('error: invalid task name:', opt.task)
         sys.exit(1)
 
-    actor = Actor(opt).apply(weights_init)
-    critic = Critic(opt).apply(weights_init)
+    actor = Actor(opt).apply(util.weights_init)
+    critic = Critic(opt).apply(util.weights_init)
     actor.cuda()
     critic.cuda()
 
@@ -207,7 +164,6 @@ if __name__ == '__main__':
 
     actor_optimizer = optim.RMSprop(actor.parameters(), lr=opt.learning_rate)
     critic_optimizer = optim.RMSprop(critic.parameters(), lr=opt.learning_rate)
-    #critic_optimizer = optim.SGD(critic.parameters(), lr=0.002) #opt.learning_rate)
 
     print('\nReal examples:')
     print(get_data(opt.batch_size, opt.seq_len, opt.vocab_size), '\n')
---
+++ util.py
@@ -0,0 +1,50 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import torch.nn as nn
+
+
+def weights_init(m):
+    def linear_init(weight):
+        fan_out, fan_in = weight.size()
+        weight.data.normal_(0.0, np.sqrt(2.0 / (fan_in + fan_out)))
+    if isinstance(m, nn.Linear):
+        linear_init(m.weight)
+    elif isinstance(m, nn.GRUCell):
+        linear_init(m.weight_ih)
+        linear_init(m.weight_hh)
+    elif isinstance(m, nn.GRU):
+        for param in m.parameters():
+            if len(param.size()) == 2:
+                linear_init(param)
+                linear_init(param)
+    elif isinstance(m, nn.Embedding):
+        m.weight.data.uniform_()
+
+
+def get_toy_data_words(batch_size, seq_len, vocab_size):
+    '''Generate very simple toy training data. Generates sequences of integers where a 'word' is
+       consecutive increasing integers and 0 separates words.'''
+    batch = np.zeros([batch_size, seq_len], dtype=np.int)
+    cur_word = np.random.randint(1, vocab_size, size=batch_size, dtype=np.int)
+    batch[:, 0] = cur_word
+    for i in xrange(1, seq_len):
+        zero_mask = cur_word == 0
+        cur_word += 1
+        cur_word[cur_word > vocab_size-1] = 0
+        cur_word *= np.random.binomial(np.ones(batch_size, dtype=np.int), 0.75)
+        cur_word[zero_mask] = np.random.randint(1, vocab_size, size=np.sum(zero_mask),
+                                                dtype=np.int)
+        batch[:, i] = cur_word
+    return batch
+
+
+def get_toy_data_longterm(batch_size, seq_len, vocab_size):
+    '''Generate simple toy training data where two tokens appear separated by large number
+       of 0's.'''
+    batch = np.zeros([batch_size, seq_len], dtype=np.int)
+    batch[:, int(0.33 * seq_len)] = np.random.randint(1, vocab_size, size=batch_size, dtype=np.int)
+    batch[:, int(0.8 * seq_len)] = np.random.randint(1, vocab_size, size=batch_size, dtype=np.int)
+    return batch
